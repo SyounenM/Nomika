eccba81b47b0820c543aa48e9de97d4d01d22bc9
hideshima <taikoud29@gmail.com>
2023-11-15 18:49:58 +0900

    変更

--- top/html/begin/group/warikan/js/test.js
+++ top/html/begin/group/warikan/js/test.js
@@ -4,7 +4,7 @@ const memberDiv = document.getElementById('member');
 function viewBuilder() {
     memberDiv.innerHTML = 'グループメンバー<br>' + memberList;
     let payerSelect = document.getElementById("payer");
-    for(member of memberList) {
+    for (member of memberList) {
         let payerOption = document.createElement("option");
         payerOption.text = member;
         payerOption.value = member;
@@ -13,42 +13,36 @@ function viewBuilder() {
 
     let resultTable = document.getElementById('result_table');
     for (member of memberList) {
-        //空の行要素を先に作成tr
         let tr = document.createElement("tr");
 
         // メンバー名
-        let nameTd = document.createElement("td");      //新しいtd要素を作って変数tdに格納
-        let nameLabel = document.createElement("label");  //tdに何か追加。
-        nameLabel.type = 'text';
+        let nameTd = document.createElement("td");
+        let nameLabel = document.createElement("label");
         nameLabel.textContent = member;
         nameLabel.id = 'memberName' + member;
-        nameTd.appendChild(nameLabel);        //tdにinpを追加
-        tr.appendChild(nameTd);         //trにtdを追加
-        
-        //支払い金額
+        nameTd.appendChild(nameLabel);
+        tr.appendChild(nameTd);
+
+        // 支払い金額
         let paymentTd = document.createElement('td');
         let paymentLabel = document.createElement('label');
-        paymentLabel.type = 'number';
         paymentLabel.id = 'payment' + member;
         paymentTd.appendChild(paymentLabel);
-        tr.appendChild(paymentTd)
+        tr.appendChild(paymentTd);
 
         // 傾斜
         let ratioTd = document.createElement('td');
-        let ratioInput = document.createElement('input');        
+        let ratioInput = document.createElement('input');
         ratioInput.type = 'number';
         ratioInput.id = 'memberRatio' + member;
         ratioInput.min = '0';
         ratioInput.max = '100';
         ratioInput.step = '1';
-        ratioInput.valueAsNumber = 1
+        ratioInput.valueAsNumber = 1;
         ratioTd.appendChild(ratioInput);
-        tr.appendChild(ratioTd)
+        tr.appendChild(ratioTd);
 
-        //完成したtrをtableに追加
         resultTable.appendChild(tr);
-
-
     }
 }
 
@@ -56,28 +50,20 @@ function calculate() {
     let memberCount = memberList.length;
     let totalAmount = parseFloat(document.getElementById('amount').value);
 
-    // 参加者の名前と割合を取得
-    let membersRatio = [];
-    for (member of memberList) {
-        let name = document.getElementById('memberName' + member).value;
-        let ratio = parseFloat(document.getElementById('memberRatio' + member).value);
-        membersRatio.push({ name: name, ratio: ratio });
-    }
-
     // 各参加者ごとの支払い金額を計算
-    let totalRatio = membersRatio.reduce((sum, member) => sum + member.ratio, 0);
+    let totalRatio = 0;
+    for (member of memberList) {
+        let ratio = parseFloat(document.getElementById('memberRatio' + member).value);
+        totalRatio += ratio;
+    }
 
     for (let i = 0; i < memberCount; i++) {
-        let amountPerPerson = (totalAmount * membersRatio[i].ratio) / totalRatio;
-        resultText += "¥" + amountPerPerson.toFixed(2);
-        document.getElementById('payment' + memberList[i]).textContent = resultText;
+        let member = memberList[i];
+        let ratio = parseFloat(document.getElementById('memberRatio' + member).value);
+        let amountPerPerson = (totalAmount * ratio) / totalRatio;
+        document.getElementById('payment' + member).textContent = "¥" + amountPerPerson.toFixed(2);
     }
-    viewBuilder();
-
 }
 
-// // 参加者の数が変更されたときに入力フィールドを更新
-// document.getElementById('submit').addEventListener('click', viewBuilder);
-
-// 初回にも一度呼び出す
+// 初回に一度呼び出す
 viewBuilder();
