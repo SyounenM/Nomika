6563507b20361c59f1d9bc28b90817c9d9579378
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-11-21 11:13:49 +0900

    modified index

--- top/html/js/index.js
+++ top/html/js/index.js
@@ -1,9 +1,8 @@
 //クラス名が「scroll-in」の要素を取得
 const objects = document.querySelectorAll('.scroll-in');
-
 //スクロール感知で実行
 const cb = function(entries, observer) {
-    entries.forEach(entry => {
+    entries.forEach(entry >;{
         if(entry.isIntersecting) {
             entry.target.classList.add('displayed');//スクロール感知で「displayed」のクラス名を付与
             observer.unobserve(entry.target); //監視の終了
@@ -16,11 +15,9 @@ const options = {
     rootMargin: "0px",
     threshold: 0
 }
-
 // IntersectionObserverインスタンス化
 const io = new IntersectionObserver(cb, options);
-
 // 監視を開始
-objects.forEach(object => {
+objects.forEach(object >;{
     io.observe(object);
 });
