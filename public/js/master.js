0bc13f0ad7e4559909b1f89413ea3a19616d160e
hideshima <taikoud29@gmail.com>
2023-12-04 19:14:09 +0900

    deploy

--- public/js/master.js
+++ public/js/master.js
@@ -1,8 +1,11 @@
+
 window.onload = function() {
     // Webページの横幅を取得
     var pageWidth = window.parent.screen.width;
-
+    console.log(1);
     // 画面の横幅をWebページの横幅に合わせる
     document.documentElement.style.width = pageWidth + 'px';
-    document.body.style.width = pageWidth + 'px';
+    document.body.style.width = pageWidth + 'px';              
 };
+
+
