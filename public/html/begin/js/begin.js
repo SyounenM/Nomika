ca1c51128c3c4d7d944e6840e5ee74a4b209f721
hideshima <taikoud29@gmail.com>
2023-12-08 16:34:11 +0900

    クエリパラメータ使用

---
+++ public/html/begin/js/begin.js
@@ -0,0 +1,23 @@
+import { app, database, ref_, set_, get_, update_, push_}  from "../../../js/master.js";
+const createButton = document.getElementById("createButton");
+
+function createGroup() {
+    const groupName = document.getElementById('groupNameInput').value;
+    const newGroupRef = push_(ref_(database, 'groups'));
+    const groupId = newGroupRef.key;
+    update_(newGroupRef, {
+        groupName: groupName
+    })
+    .then(()=>{
+        console.log("データが正常に書き込まれました");
+    })
+    .catch((error)=>{
+        console.error("データの書き込みに失敗しました", error);
+    })
+    const groupURL = window.location.href + 'group.html?id=' + groupId;
+    alert('グループが作成されました。\nURL: ' + groupURL);
+    createButton.href = `./group/group.html?id=${newGroupRef.key}`;
+}
+
+createButton.onclick = createGroup;
+
--- public/js/master.js
+++ public/js/master.js
@@ -12,7 +12,7 @@ window.onload = function() {
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
-import { getDatabase, ref, get, set, update} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase, ref, get, set, update, push} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
     // Your web app's Firebase configuration
@@ -44,6 +44,9 @@ export function set_(ref, json) {
 export function update_(ref, json) {
     return update(ref, json);
 }
+export function push_(ref) {
+    return push(ref);
+}
 //使用例
 // console.log(1);
 
@@ -57,17 +60,17 @@ export function update_(ref, json) {
 // console.error("データの読み取りに失敗しました", error);
 // });
 
-// const dataRef2 = ref(database, "name");
-// // データを書き込む
-// update_(dataRef2, {
-//     key1: "value1",
-//     key2: "value2",
-//     // 他のデータ
-// })
-//     .then(() => {
-//     console.log("データが正常に書き込まれました");
-//     })
-//     .catch((error) => {
-//     console.error("データの書き込みに失敗しました", error);
-//     });
+const dataRef2 = ref(database, "name2");
+// データを書き込む
+update_(dataRef2, {
+    key1: "value1",
+    key2: "value2",
+    // 他のデータ
+})
+    .then(() => {
+    console.log("データが正常に書き込まれました");
+    })
+    .catch((error) => {
+    console.error("データの書き込みに失敗しました", error);
+    });
 
