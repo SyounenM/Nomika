62dec8590e94b14b773141e977552baaee05ad53
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-12-13 12:55:32 +0900

    modifeid begin 文字被るやつ

--- public/html/begin/js/begin.js
+++ public/html/begin/js/begin.js
@@ -1,5 +1,33 @@
 import { app, database, ref_, set_, get_, update_, push_}  from "../../../js/master.js";
 const createButton = document.getElementById("createButton");
+const inputName = document.getElementById("inputName");
+const inputGroup = document.getElementById("groupNameInput");
+const inputMember = document.getElementById("inputMember");
+const labelName = document.getElementById("labelName");
+const labelGroup = document.getElementById("labelGroup");
+const labelMember = document.getElementById("labelMember");
+
+inputName.addEventListener("input", function() {
+    if (inputName.value !== "") {
+        labelName.innerHTML = "";
+    } else {
+        labelName.innerHTML = "<h5>お名前</h5>";
+    }
+});
+inputGroup.addEventListener("input", function() {
+    if (inputGroup.value !== "") {
+        labelGroup.innerHTML = "";
+    } else {
+        labelGroup.innerHTML = "<h5>グループ名</h5>";
+    }
+});
+inputMember.addEventListener("input", function() {
+    if (inputMember.value !== "") {
+        labelMember.innerHTML = "";
+    } else {
+        labelMember.innerHTML = "<h5>メンバー名</h5>";
+    }
+});
 
 function createGroup() {
     const groupName = document.getElementById('groupNameInput').value;
