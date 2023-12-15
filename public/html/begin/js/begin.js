0fbf730c6dba9c5f6e02d70a5d7edb0d2f434d7f
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-12-15 16:35:40 +0900

    modified begin

--- public/html/begin/js/begin.js
+++ public/html/begin/js/begin.js
@@ -1,9 +1,9 @@
 import { app, database, ref_, set_, get_, update_, push_}  from "../../../js/master.js";
 const createButton = document.getElementById("createButton");
-const inputName = document.getElementById("inputName");
+// const inputName = document.getElementById("inputName");
 const inputGroup = document.getElementById("groupNameInput");
 const inputMember = document.getElementById("inputMember");
-const labelName = document.getElementById("labelName");
+// const labelName = document.getElementById("labelName");
 const labelGroup = document.getElementById("labelGroup");
 const labelMember = document.getElementById("labelMember");
 const addButton = document.getElementById("addButton");
@@ -11,13 +11,13 @@ let memberDiv = document.getElementById('memberList');
 let member;
 let memberList = [];
 
-inputName.addEventListener("input", function() {
-    if (inputName.value !== "") {
-        labelName.innerHTML = "";
-    } else {
-        labelName.innerHTML = "<h5>お名前</h5>";
-    }
-});
+// inputName.addEventListener("input", function() {
+//     if (inputName.value !== "") {
+//         labelName.innerHTML = "";
+//     } else {
+//         labelName.innerHTML = "<h5>お名前</h5>";
+//     }
+// });
 inputGroup.addEventListener("input", function() {
     if (inputGroup.value !== "") {
         labelGroup.innerHTML = "";
@@ -69,29 +69,30 @@ function cancelMember(member) {
 }
 
 function createGroup() {
-    const groupName = document.getElementById('groupNameInput').value;
-    const newGroupRef = push_(ref_(database, 'groups'));
-    const groupId = newGroupRef.key;
-    set_(newGroupRef, {
-        groupName: groupName,
-        groupMember: memberList
-    })
-    .then(()=>{
-        console.log("データが正常に書き込まれました");
-        console.log(memberList);
-        const groupURL = window.location.href + 'group.html?id=' + groupId;
-        alert('グループが作成されました。\nURL: ' + groupURL);
-        // createButton.href = `./group/group.html?id=${newGroupRef.key}`;
-        // 新しいURLに遷移する
-        window.location.href = `./group/group.html?id=${newGroupRef.key}`;
+    if (memberList.length != 0) {
+        const groupName = document.getElementById('groupNameInput').value;
+        const newGroupRef = push_(ref_(database, 'groups'));
+        const groupId = newGroupRef.key;
+        set_(newGroupRef, {
+            groupName: groupName,
+            groupMember: memberList
+        })
+        .then(()=>{
+            console.log("データが正常に書き込まれました");
+            console.log(memberList);
+            const groupURL = window.location.href + 'group.html?id=' + groupId;
+            alert('グループが作成されました。\nURL: ' + groupURL);
+            // createButton.href = `./group/group.html?id=${newGroupRef.key}`;
+            // 新しいURLに遷移する
+            window.location.href = `./group/group.html?id=${newGroupRef.key}`;
 
-    })
-    .catch((error)=>{
-        console.error("データの書き込みに失敗しました", error);
-    })
-
-
-
+        })
+        .catch((error)=>{
+            console.error("データの書き込みに失敗しました", error);
+        })
+    } else {
+        alert('メンバーを追加してください');
+    }
 }
 
 createButton.onclick = createGroup;
