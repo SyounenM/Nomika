27fb24702f15060bf6d96782a63406070b4517f3
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2023-12-15 18:17:14 +0900

    modified edit

--- public/html/begin/group/edit/js/edit.js
+++ public/html/begin/group/edit/js/edit.js
@@ -24,60 +24,85 @@ function showAlert() {
 topButton.onclick = showAlert;
 home.href = `../group.html?id=${groupId}`;
 cancelButton.href = `../group.html?id=${groupId}`;
-updateButton.href = `../group.html?id=${groupId}`;
 
 
 
 
 // 定数（データベースで共有した情報)
-let groupName = "飲み会"
-let memberList = ["秀島", "川崎", "佐々木", "福田", "松島"];
-
-let memberTable = document.getElementById('memberTable');
-let memberListDiv = document.getElementById('memberList');
-
-function viewBuilder() {
-    let groupNameBox = document.getElementById('groupNameBox');
-    groupNameBox.value = groupName; 
-    for (let i=0 ; i<memberList.length; i++) {
-        createMember(memberList[i]);
+let memberList = [];
+const groupMemberRef = ref_(database, 'groups/' + groupId + '/groupMember');
+get_(groupMemberRef)
+    .then((snapshot) => {
+    if (snapshot.exists()) {
+        memberList = snapshot.val();
+        console.log(memberList);
+        displayMembers();
+    } else {
+        console.log('データが存在しません');
     }
-    //完成したtrをtableに追加
-    memberTable.appendChild(tr);  
+    })
+    .catch((error) => {
+    console.error('データの取得に失敗:', error);
+});
+
+let memberDiv = document.getElementById('memberList');
+
+function displayMembers() {
+    for (let i = 0; i < memberList.length; i++) {
+        let member = memberList[i];
+        console.log(member);
+        
+        let memberSpan = document.createElement("span");
+        memberSpan.type = 'text';
+        memberSpan.textContent = member;
+        memberSpan.style = 'font-size: 25px; height: 50px; background-color:white; margin-right:10px; border: solid 1px black; border-width: 2px; border-radius: 10px; padding: 7px;';
+        memberSpan.id = member + "Span";
+
+        let cancelButton = document.createElement('button');
+        cancelButton.textContent = "×";
+        cancelButton.id = member + 'Cancel';
+        cancelButton.style = 'font-size: 15px; position: relative; top: -3px; width: 35px; background-color:white; border: solid 1px black; border-width: 2px; border-radius: 10px; padding: 5px; margin-left: 10px;';
+
+        cancelButton.onclick = function(){
+            cancelMember(member);
+        };
+
+        memberSpan.appendChild(cancelButton);
+        memberDiv.appendChild(memberSpan);
+    }
 }
 
+const inputMember = document.getElementById("inputMember");
 
-function addMember() {
-    let newMember = document.getElementById('newMember').value;
-    memberList.push(newMember);
-    createMember(newMember);
-    console.log(memberList);
-}
+addButton.addEventListener("click", function() {
+    if (inputMember.value != "") {
+        let member;
+        member = inputMember.value;
+        memberList.push(member);
+        let memberSpan = document.createElement("span");
+        memberSpan.type = 'text';
+        memberSpan.textContent =  '' + member + '' ;
+        memberSpan.style = 'font-size: 25px; height: 50px; background-color:white; margin-right:10px; border: solid 1px black; border-width: 2px; border-radius: 10px; padding: 7px;';
+        memberSpan.id = member + "Span";
 
+        let cancelButton = document.createElement('button');
+        cancelButton.textContent = "×";
+        cancelButton.id = member + 'Cancel';
+        cancelButton.style = 'font-size: 15px; position: relative; top: -3px; width: 35px; background-color:white; border: solid 1px black; border-width: 2px; border-radius: 10px; padding: 5px; margin-left: 10px;';
+        // cancelButton.style.height = '15px';
+        // cancelButton.style.width = '15px';
+        // cancelButton.style.offset
 
-function createMember(member) {
-    // メンバー名
-    let memberSpan = document.createElement("span");  //tdに何か追加。
-    memberSpan.type = 'text';
-    memberSpan.textContent =  ''+ member + '' ;
-    memberSpan.style = 'background-color:white; margin-right:10px; border: solid 1px black; border-width: 2px; border-radius: 10px; padding: 5px;';
-    memberSpan.id = member + "Span";
+        cancelButton.onclick = function(){
+            cancelMember(member);
+        };
 
-    let cancelButton = document.createElement('button');
-    cancelButton.textContent = "×";
-    cancelButton.id = member + 'Cancel';
-    cancelButton.style = 'font-size: 15px; position: relative; top: -3px; width: 35px; background-color:white; border: solid 1px black; border-width: 2px; border-radius: 10px; padding: 5px; margin-left: 10px;';
-    // cancelButton.style.height = '15px';
-    // cancelButton.style.width = '15px';
-    // cancelButton.style.offset
-
-    cancelButton.onclick = function(){
-        cancelMember(member);
-    };
-
-    memberSpan.appendChild(cancelButton)
-    memberListDiv.appendChild(memberSpan);  
-}
+        memberSpan.appendChild(cancelButton)
+        memberDiv.appendChild(memberSpan); 
+    } else {
+        alert('メンバー名を入力してください');
+    }
+});
 
 function cancelMember(member) {
     console.log(memberList);
@@ -88,12 +113,43 @@ function cancelMember(member) {
     target.remove();
 }
 
-function update(params) {
-    
+function updateMembers() {
+    if (memberList.length != 0) {
+        let groupName;
+        const groupNameRef = ref_(database, 'groups/' + groupId + '/groupName');
+        const groupRef = ref_(database, 'groups/' + groupId);
+        get_(groupNameRef)
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                groupName = snapshot.val();
+                console.log('データ:', groupName);
+                } else {
+                console.log('データが存在しません');
+                }
+            })
+            .catch((error) => {
+                console.error('データの取得に失敗:', error);
+            });
+        set_(groupRef, {
+            groupName: groupName,
+            groupMember: memberList
+        })
+        .then(()=>{
+            console.log("データが正常に書き込まれました");
+            console.log(memberList);
+            // const groupURL = window.location.href + 'group.html?id=' + groupId;
+            // alert('グループが作成されました。\nURL: ' + groupURL);
+            // createButton.href = `./group/group.html?id=${newGroupRef.key}`;
+            // 新しいURLに遷移する
+            window.location.href = `../group.html?id=${groupId}`;
+
+        })
+        .catch((error)=>{
+            console.error("データの書き込みに失敗しました", error);
+        })
+    } else {
+        alert('メンバーを追加してください');
+    }
 }
 
-function cancel(params) {
-    
-}
-
-viewBuilder();
+updateButton.onclick = updateMembers;
