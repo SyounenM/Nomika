b6f2863040c39af0086ebd9aa6180c4684585314
hideshima <taikoud29@gmail.com>
2024-01-19 17:05:33 +0900

    表固定

--- public/js/master.js
+++ public/js/master.js
@@ -12,7 +12,7 @@ window.onload = function() {
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
-import { getDatabase, ref, get, set, update, push, goOffline} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase, ref, get, set, update, push, goOffline,remove} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
     // Your web app's Firebase configuration
@@ -70,6 +70,9 @@ export function push_(ref) {
 }
 export function goOffline_(database) {
     return goOffline(database);
+}
+export function remove_(ref){
+    return remove(ref);
 }
 //使用例
 // console.log(1);
